627a4d5f1dedeb9ac27eeb6a129da424904c249c
TaxStat360 <natasha.verela@gmail.com>
2026-06-02 11:19:55 -0400

    Update cache version from v5 to v6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='repsrecord-v5';
+const CACHE='repsrecord-v6';
 const STATIC=['icon-192x192.png','icon-512x512.png','apple-touch-icon.png','manifest.json'];
 
 self.addEventListener('install',e=>{
